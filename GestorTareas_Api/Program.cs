4a4120ac9a4cca437d7c1d71230cb1a7b4e38ab0
Jeremy Mora <morajere777@gmail.com>
2025-04-12 04:55:01 -0600

    Revert "Cambio Usuarios Enq Base de Datos"
    This reverts commit 65f8976489a776bd35381e46e5217f08e1f4f78a.

--- GestorTareas_Api/Program.cs
+++ GestorTareas_Api/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSql")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GestorTareasDb")));
 
 var app = builder.Build();
 
